6b7c0927f71eed4ba1311163b4a58fcc812932b2
Komahan Boopathy <komahan.cool@gmail.com>
2020-02-02 07:13:26 -0500

    setting up wrapped for stacs

---
+++ stacs/cwrap.pxd
@@ -0,0 +1,2 @@
+# Typdefs required for either real or complex mode
+#include "TacsTypedefs.pxi"
---
+++ stacs/cwrap.pyx
@@ -0,0 +1,8 @@
+# distutils: language = c++
+from cwrap cimport *
+
+# Import numpy
+cimport numpy as np
+import numpy as np
+
+#include "TacsDefs.pxi"
