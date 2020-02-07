1412febc93302621e79fe6a0592d7fe76e261990
komahan <komahan.cool@gmail.com>
2020-02-07 13:05:50 -0500

    commented out wrapping because of erroneous import during runtime

--- stacs/STACS.pxd
+++ stacs/STACS.pxd
@@ -1,22 +1,16 @@
+# Include the mpi4py header
+cdef extern from "mpi-compat.h":
+    pass
+
 # Typdefs required for either real or complex mode
-include "TacsTypedefs.pxi"
+#include "TacsTypedefs.pxi"
 
 #from TACS cimport *
-from pspace.PSPACE cimport *
-from tacs.TACS cimport *
 from tacs.elements cimport *
-
-## cdef extern from "TACSElement.h":
-##     cdef cppclass TACSElement:
-##         pass
-
-## cdef extern from "ParameterContainer.h":
-##     cdef cppclass ParameterContainer:
-##         pass
+from pspace.PSPACE cimport *
 
 cdef extern from "TACSStochasticElement.h":
     cdef cppclass TACSStochasticElement(TACSElement):
         TACSStochasticElement( TACSElement *_delem,
                                ParameterContainer *_pc,
                                void (*_update)(TACSElement*, TacsScalar*) )
-
--- stacs/STACS.pyx
+++ stacs/STACS.pyx
@@ -1,44 +1,21 @@
 # distutils: language = c++
 from __future__ import print_function, division
 
-# Include the mpi4py header
-cdef extern from "mpi-compat.h":
-    pass
-
-from mpi4py.libmpi cimport *
-cimport mpi4py.MPI as MPI
-
-# Import numpy
-cimport numpy as np
-import numpy as np
-np.import_array()
-
-#from TACS cimport *
-from pspace.PSPACE cimport *
-from tacs.TACS cimport *
-from tacs.elements cimport *
-from STACS cimport *
-
-
 # Import TACS
-include "TacsDefs.pxi"
-
-# Import the definition required for const strings
-from libc.string cimport const_char
-from libc.stdlib cimport malloc, free
+# include "TacsDefs.pxi"
 
 # Import C methods for python
 from cpython cimport PyObject, Py_INCREF
 
-cdef class PyStochasticElement(Element):
-    def __cinit__(self, Element elem):
-        cdef TACSStochasticElement *sptr
-        sptr = new TACSStochasticElement(elem.ptr, NULL, NULL)
-        #self.ptr = sptr
-        #self.ptr.incref()
-        return
+## cdef class PyStochasticElement(Element):
+##     cdef TACSStochasticElement *sptr
+##     def __cinit__(self, Element elem):#, PyParameterContainer pc):
+##         self.sptr = new TACSStochasticElement(elem.ptr, NULL, NULL)
+##         self.ptr = self.sptr
+##         self.ptr.incref()
+##         return
 
-    def __dealloc__(self):
-        if self.ptr:
-            self.ptr.decref()
-        return
+##     def __dealloc__(self):
+##         if self.ptr:
+##             self.ptr.decref()
+##         return
