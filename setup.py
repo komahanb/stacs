432c551a7bc0c2fa8eb030867e5ca225fc660885
komahan <komahan.cool@gmail.com>
2020-03-21 18:19:57 -0400

    committed files

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ import os
 from subprocess import check_output
 import sys
 
+# Numpy/mpi4py must be installed prior to installing TACS
+import numpy
+import mpi4py
+import pspace
+import tacs
+import tmr
+
 # Import distutils
 from setuptools import setup
 from distutils.core import Extension as Ext
 from Cython.Build import cythonize
 
-# Numpy/mpi4py must be installed prior to installing TACSUQ
-import numpy
-import mpi4py
-import tacs
-import pspace
-
 # Convert from local to absolute directories
 def get_global_dir(files):
-    tacsuq_root = os.path.abspath(os.path.dirname(__file__))
+    tacs_root = os.path.abspath(os.path.dirname(__file__))
     new = []
     for f in files:
-        new.append(os.path.join(tacsuq_root, f))
+        new.append(os.path.join(tacs_root, f))
     return new
 
 def get_mpi_flags():
@@ -39,44 +40,53 @@ def get_mpi_flags():
 
 inc_dirs, lib_dirs, libs = get_mpi_flags()
 
-# Add tacsuq-dev/lib as a runtime directory
-runtime_lib_dirs = get_global_dir(['cpp'])
+# Add the numpy/mpi4py directories
+inc_dirs.extend([numpy.get_include(), mpi4py.get_include()])
 
-# add include dirs
-inc_dirs.extend(tacs.get_include())
+# PSPACE
 inc_dirs.extend(pspace.get_include())
-inc_dirs.extend(tacs.get_cython_include())
 inc_dirs.extend(pspace.get_cython_include())
-inc_dirs.extend([numpy.get_include()])
-inc_dirs.extend([mpi4py.get_include()])
-
-# add library dirs
-lib_dirs.extend(tacs.get_libraries()[0])
 lib_dirs.extend(pspace.get_libraries()[0])
+libs.extend(pspace.get_libraries()[1])
 
-libs.extend(['pspace', 'tacs',
-             'stacs'])
+# STACS (local cpp files)
+inc_dirs.extend(get_global_dir(['cpp']))
+lib_dirs.extend(get_global_dir(['cpp']))
+libs.extend(['stacs'])
 
-# Convert from relative to absolute directories
-rel_inc_dirs = ['cpp']
-rel_lib_dirs = ['cpp']
-inc_dirs.extend(get_global_dir(rel_inc_dirs))
-lib_dirs.extend(get_global_dir(rel_lib_dirs))
+# TACS
+inc_dirs.extend(tacs.get_include())
+inc_dirs.extend(tacs.get_cython_include())
+lib_dirs.extend(tacs.get_libraries()[0])
+libs.extend(tacs.get_libraries()[1])
 
-#print(inc_dirs)
-#print(lib_dirs)
-#print(libs)
+# TMR
+inc_dirs.extend(tmr.get_include())
+inc_dirs.extend(tmr.get_cython_include())
+lib_dirs.extend(tmr.get_libraries()[0])
+libs.extend(tmr.get_libraries()[1])
+
+# The provide where the run time libraries are present
+runtime_lib_dirs = [] 
+runtime_lib_dirs.extend(lib_dirs)
+
+print(inc_dirs)
+print(libs)
+print(lib_dirs)
+print(runtime_lib_dirs)
 
 exts = []
 for mod in ['STACS']:
     exts.append(Ext('stacs.%s'%(mod), sources=['stacs/%s.pyx'%(mod)],
-                    include_dirs=inc_dirs, libraries=libs, 
-                    library_dirs=lib_dirs, runtime_library_dirs=runtime_lib_dirs,
+                    include_dirs=inc_dirs,
+                    libraries=libs, 
+                    library_dirs=lib_dirs,
+                    runtime_library_dirs=runtime_lib_dirs,
                     cython_directives={"embedsignature": True, "binding": True}))
 
 setup(name='stacs',
       version=1.0,
-      description='Stochastic TACS : UQ/OUU plugin for TACS',
+      description='Stochastic TACS -- Extension of TACS',
       author='Komahan Boopathy',
       author_email='komibuddy@gmail.com',
       ext_modules=cythonize(exts, include_path=inc_dirs))
--- stacs/STACS.pxd
+++ stacs/STACS.pxd
@@ -1,16 +1,57 @@
+# Typdefs required for either real or complex mode
+include "TacsTypedefs.pxi"
+
 # Include the mpi4py header
 cdef extern from "mpi-compat.h":
     pass
 
-# Typdefs required for either real or complex mode
-#include "TacsTypedefs.pxi"
-
 #from TACS cimport *
-from tacs.elements cimport *
 from pspace.PSPACE cimport *
+from tacs.elements cimport *
 
 cdef extern from "TACSStochasticElement.h":
     cdef cppclass TACSStochasticElement(TACSElement):
         TACSStochasticElement( TACSElement *_delem,
                                ParameterContainer *_pc,
-                               void (*_update)(TACSElement*, TacsScalar*) )
+                               void (*_update)(TACSElement*, TacsScalar*, void*) )
+        TACSElement* getDeterministicElement()
+        void updateElement(TACSElement* elem, TacsScalar* vals)
+        void setPythonCallback(PyObject *cbptr)
+        
+cdef cppclass PyStochasticElement(Element):
+    cdef TACSStochasticElement *ptr
+
+## cdef extern from "TACSMutableElement3D.h":
+##     cdef cppclass TACSMutableElement3D(TACSElement):
+##         TACSMutableElement3D( TACSElement *_elem )
+##         void setDensity( TacsScalar _rho )
+
+## # A simple test element for TACS
+## cdef extern from "smd.h":
+##     cdef cppclass SMD(TACSElement):
+##         SMD( TacsScalar, TacsScalar, TacsScalar)
+##         void setMass(TacsScalar)
+##         void setStiffness(TacsScalar)
+##         void setDamping(TacsScalar)
+    
+## from tacs.functions cimport *
+
+## cdef extern from "TACSStochasticFunction.h":
+##     cdef cppclass TACSStochasticFunction(TACSFunction):
+##         TACSStochasticFunction( TACSAssembler *tacs,
+##                                 TACSFunction *dfunc,
+##                                 ParameterContainer *pc,
+##                                 int quantityType,
+##                                 int moment_type )
+##         TacsScalar getFunctionValue()
+    
+## cdef extern from "TACSKSStochasticFunction.h":
+##     cdef cppclass TACSKSStochasticFunction(TACSFunction):
+##         TACSKSStochasticFunction( TACSAssembler *tacs,
+##                                   TACSFunction *dfunc,
+##                                   ParameterContainer *pc,
+##                                   int quantityType,
+##                                   int moment_type,
+##                                   int ksweight)
+##         TacsScalar getFunctionValue()
+  
