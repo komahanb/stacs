c521c6ab0f9880cf2e49a72e898abf5f39294297
komahan <komahan.cool@gmail.com>
2020-03-21 19:03:48 -0400

    fixed compilation of wrapper modules

--- stacs/STACS.pxd
+++ stacs/STACS.pxd
@@ -8,50 +8,57 @@ cdef extern from "mpi-compat.h":
 #from TACS cimport *
 from pspace.PSPACE cimport *
 from tacs.elements cimport *
+from tacs.functions cimport *
 
-## cdef extern from "TACSStochasticElement.h":
-##     cdef cppclass TACSStochasticElement(TACSElement):
-##         TACSStochasticElement( TACSElement *_delem,
-##                                ParameterContainer *_pc,
-##                                void (*_update)(TACSElement*, TacsScalar*, void*) )
-##         TACSElement* getDeterministicElement()
-##         void updateElement(TACSElement* elem, TacsScalar* vals)
-##         void setPythonCallback(PyObject *cbptr)
+# C++ class TACSStochasticElement
+cdef extern from "TACSStochasticElement.h":
+    cdef cppclass TACSStochasticElement(TACSElement):
+        TACSStochasticElement( TACSElement *_delem,
+                               ParameterContainer *_pc,
+                               void (*_update)(TACSElement*, TacsScalar*, void*) )
+        TACSElement* getDeterministicElement()
+        void updateElement(TACSElement* elem, TacsScalar* vals)
+        void setPythonCallback(PyObject *cbptr)
 
-## cdef class PyStochasticElement(Element):
-##     cdef TACSStochasticElement *sptr
+# Python wrapped class to C++ class TACSStochasticElement
+cdef class PyStochasticElement(Element):
+    cdef TACSStochasticElement *sptr
 
-## cdef extern from "TACSMutableElement3D.h":
-##     cdef cppclass TACSMutableElement3D(TACSElement):
-##         TACSMutableElement3D( TACSElement *_elem )
-##         void setDensity( TacsScalar _rho )
+# C++ class TACSMutableElement3D
+cdef extern from "TACSMutableElement3D.h":
+    cdef cppclass TACSMutableElement3D(TACSElement):
+        TACSMutableElement3D( TACSElement *_elem )
+        void setDensity( TacsScalar _rho )
 
-## # A simple test element for TACS
-## cdef extern from "smd.h":
-##     cdef cppclass SMD(TACSElement):
-##         SMD( TacsScalar, TacsScalar, TacsScalar)
-##         void setMass(TacsScalar)
-##         void setStiffness(TacsScalar)
-##         void setDamping(TacsScalar)
-    
-## from tacs.functions cimport *
+# Python wrapped C++ class TACSMutableElement3D
+cdef class MutableElement3D(Element):
+    cdef TACSMutableElement3D *sptr    
 
-## cdef extern from "TACSStochasticFunction.h":
-##     cdef cppclass TACSStochasticFunction(TACSFunction):
-##         TACSStochasticFunction( TACSAssembler *tacs,
-##                                 TACSFunction *dfunc,
-##                                 ParameterContainer *pc,
-##                                 int quantityType,
-##                                 int moment_type )
-##         TacsScalar getFunctionValue()
-    
-## cdef extern from "TACSKSStochasticFunction.h":
-##     cdef cppclass TACSKSStochasticFunction(TACSFunction):
-##         TACSKSStochasticFunction( TACSAssembler *tacs,
-##                                   TACSFunction *dfunc,
-##                                   ParameterContainer *pc,
-##                                   int quantityType,
-##                                   int moment_type,
-##                                   int ksweight)
-##         TacsScalar getFunctionValue()
-  
+# C++ class     
+cdef extern from "TACSStochasticFunction.h":
+    cdef cppclass TACSStochasticFunction(TACSFunction):
+        TACSStochasticFunction( TACSAssembler *tacs,
+                                TACSFunction *dfunc,
+                                ParameterContainer *pc,
+                                int quantityType,
+                                int moment_type )
+        TacsScalar getFunctionValue()
+        
+# Python wrapped class
+cdef class PyMomentSpaceTimeIntegral(Function):
+    cdef TACSStochasticFunction *sptr
+
+# C++ class 
+cdef extern from "TACSKSStochasticFunction.h":
+    cdef cppclass TACSKSStochasticFunction(TACSFunction):
+        TACSKSStochasticFunction( TACSAssembler *tacs,
+                                  TACSFunction *dfunc,
+                                  ParameterContainer *pc,
+                                  int quantityType,
+                                  int moment_type,
+                                  int ksweight)
+        TacsScalar getFunctionValue()
+
+# Python wrapped class
+cdef class PyMomentMaxSpaceTimeIntegral(Function):
+    cdef TACSKSStochasticFunction *sptr
--- stacs/STACS.pyx
+++ stacs/STACS.pyx
@@ -1,5 +1,5 @@
 # distutils: language = c++
-from PSPACE cimport *
+from STACS cimport *
 
 # Import numpy
 cimport numpy as np
@@ -13,120 +13,92 @@ include "TacsDefs.pxi"
 
 cdef inplace_array_1d(int nptype, int dim1, void *data_ptr):
     '''Return a numpy version of the array'''
-    # Set the shape of the array
     cdef int size = 1
     cdef np.npy_intp shape[1]
     cdef np.ndarray ndarray
-
-    # Set the first entry of the shape array
     shape[0] = <np.npy_intp>dim1
-
-    # Create the array itself - Note that this function will not
-    # delete the data once the ndarray goes out of scope
     ndarray = np.PyArray_SimpleNewFromData(size, shape, nptype, data_ptr)
-
     return ndarray
 
-## cdef void updateCB(TACSElement *elem, TacsScalar *yvals, void *pyptr):
-##     _yvals = inplace_array_1d(TACS_NPY_SCALAR, 5, <void*> yvals)
-##     (<object>pyptr).update(_yvals)
-##     return
+cdef void updateCB(TACSElement *elem, TacsScalar *yvals, void *pyptr):
+    _yvals = inplace_array_1d(TACS_NPY_SCALAR, 5, <void*> yvals)
+    (<object>pyptr).update(_yvals)
+    return
 
-## cdef class PyStochasticElement(Element):
-##     def __cinit__(self, Element elem,
-##                   PyParameterContainer pc,
-##                   update):
-##         self.sptr = new TACSStochasticElement(elem.ptr, pc.ptr, &updateCB)
-##         self.sptr.incref()        
-##         self.sptr.setPythonCallback(<PyObject*>update)
-##         self.ptr = self.sptr
-##         Py_INCREF(update)
-##         Py_INCREF(self)
-##     def __dealloc__(self):        
-##         if self.sptr:
-##             self.sptr.decref()
-##             Py_DECREF(self)
-##     def getDeterministicElement(self):
-##         delem = Element()
-##         delem.ptr = self.sptr.getDeterministicElement() 
-##         delem.ptr.incref()
-##         return delem
-##     def updateElement(self, Element elem, np.ndarray[TacsScalar, ndim=1, mode='c'] vals):
-##         self.sptr.updateElement(elem.ptr, <TacsScalar*> vals.data)
-##     def setPythonCallback(self, cb):
-##         self.sptr.setPythonCallback(<PyObject*>cb)
+cdef class PyStochasticElement(Element):
+    def __cinit__(self, Element elem,
+                  PyParameterContainer pc,
+                  update):
+        self.sptr = new TACSStochasticElement(elem.ptr, pc.ptr, &updateCB)
+        self.sptr.incref()        
+        self.sptr.setPythonCallback(<PyObject*>update)
+        self.ptr = self.sptr
+        Py_INCREF(update)
+        Py_INCREF(self)
+    def __dealloc__(self):        
+        if self.sptr:
+            self.sptr.decref()
+            Py_DECREF(self)
+    def getDeterministicElement(self):
+        delem = Element()
+        delem.ptr = self.sptr.getDeterministicElement() 
+        delem.ptr.incref()
+        return delem
+    def updateElement(self, Element elem, np.ndarray[TacsScalar, ndim=1, mode='c'] vals):
+        self.sptr.updateElement(elem.ptr, <TacsScalar*> vals.data)
+    def setPythonCallback(self, cb):
+        self.sptr.setPythonCallback(<PyObject*>cb)
 
-## cdef class PySMD(Element):
-##     cdef SMD *smd
-##     def __cinit__(self, TacsScalar m, TacsScalar c, TacsScalar k):
-##         self.smd = new SMD(m, c, k)
-##         self.ptr = self.smd
-##         self.ptr.incref()
-##         Py_INCREF(self)
-##     def __dealloc__(self):
-##         if self.ptr:
-##             self.ptr.decref()
-##             Py_DECREF(self)
-##     def setMass(self, TacsScalar m):
-##         return self.smd.setMass(m)
-##     def setStiffness(self, TacsScalar k):
-##         return self.smd.setStiffness(k)
-##     def setDamping(self, TacsScalar c):
-##         return self.smd.setDamping(c)
+cdef class MutableElement3D(Element):
+    def __cinit__(self, Element elem):
+        self.sptr = new TACSMutableElement3D(elem.ptr)
+        self.ptr = self.sptr
+        self.ptr.incref()
+        return
+    def __dealloc__(self):        
+        if self.ptr:
+            self.ptr.decref()
+    def setDensity(self, int rho):
+        self.sptr.setDensity(rho)
 
-## cdef class MutableElement3D(Element):
-##     cdef TACSMutableElement3D *sptr    
-##     def __cinit__(self, Element elem):
-##         self.sptr = new TACSMutableElement3D(elem.ptr)
-##         self.ptr = self.sptr
-##         self.ptr.incref()
-##         return
-##     def __dealloc__(self):        
-##         if self.ptr:
-##             self.ptr.decref()
-##     def setDensity(self, int rho):
-##         self.sptr.setDensity(rho)
-
-## cdef class PyMomentSpaceTimeIntegral(Function):
-##     cdef TACSStochasticFunction *sptr
-##     def __cinit__(self,
-##                   Assembler assembler, Function func, PyParameterContainer pc,
-##                   int quantity_type, int moment_type):
-##         self.sptr = new TACSStochasticFunction( assembler.ptr, func.ptr, pc.ptr,
-##                                                 quantity_type, moment_type )
-##         self.sptr.incref()        
-##         self.ptr = self.sptr
-##         Py_INCREF(self) #? do we need this?
-##         return
+cdef class PyMomentSpaceTimeIntegral(Function):
+    def __cinit__(self,
+                  Assembler assembler, Function func, PyParameterContainer pc,
+                  int quantity_type, int moment_type):
+        self.sptr = new TACSStochasticFunction( assembler.ptr, func.ptr, pc.ptr,
+                                                quantity_type, moment_type )
+        self.sptr.incref()        
+        self.ptr = self.sptr
+        Py_INCREF(self) #? do we need this?
+        return
     
-##     def __dealloc__(self):        
-##         if self.sptr:
-##             self.sptr.decref()
-##             Py_DECREF(self)  #? do we need this?
-##         return
+    def __dealloc__(self):        
+        if self.sptr:
+            self.sptr.decref()
+            Py_DECREF(self)  #? do we need this?
+        return
 
-##     def getFunctionValue(self):
-##         return self.sptr.getFunctionValue()
+    def getFunctionValue(self):
+        return self.sptr.getFunctionValue()
 
-## cdef class PyMomentMaxSpaceTimeIntegral(Function):
-##     cdef TACSKSStochasticFunction *sptr
-##     def __cinit__(self,
-##                   Assembler assembler, Function func, PyParameterContainer pc,
-##                   int quantity_type, int moment_type,
-##                   int ksweight):
-##         self.sptr = new TACSKSStochasticFunction( assembler.ptr, func.ptr, pc.ptr,
-##                                                   quantity_type, moment_type,
-##                                                   ksweight)
-##         self.sptr.incref()        
-##         self.ptr = self.sptr
-##         Py_INCREF(self) #? do we need this?
-##         return
+cdef class PyMomentMaxSpaceTimeIntegral(Function):
+    def __cinit__(self,
+                  Assembler assembler, Function func, PyParameterContainer pc,
+                  int quantity_type, int moment_type,
+                  int ksweight):
+        self.sptr = new TACSKSStochasticFunction( assembler.ptr, func.ptr, pc.ptr,
+                                                  quantity_type, moment_type,
+                                                  ksweight)
+        self.sptr.incref()        
+        self.ptr = self.sptr
+        Py_INCREF(self) #? do we need this?
+        return
     
-##     def __dealloc__(self):        
-##         if self.sptr:
-##             self.sptr.decref()
-##             Py_DECREF(self)  #? do we need this?
-##         return
+    def __dealloc__(self):        
+        if self.sptr:
+            self.sptr.decref()
+            Py_DECREF(self)  #? do we need this?
+        return
 
-##     def getFunctionValue(self):
-##         return self.sptr.getFunctionValue()
+    def getFunctionValue(self):
+        return self.sptr.getFunctionValue()
